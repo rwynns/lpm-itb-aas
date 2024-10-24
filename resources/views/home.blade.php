3d97fa9cb83ec8f764623d57e358bdb747241bb7
rwynns <erwintra88@gmail.com>
2024-10-24 19:43:55 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="hero">
+        <div class="hero-content">
+            <h1>LEMBAGA PENJAMINAN MUTU</h1>
+            <p>INSTITUT TEKNOLOGI BISNIS AAS INDONESIA</p>
+        </div>
+    </div>
+    <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-primary">Email</button>
+        <button type="button" class="btn btn-primary">Lokasi</button>
+        <button type="button" class="btn btn-primary">Telepon</button>
+    </div>
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg transparent-navbar">
+    <div class="container-fluid">
+        <a class="navbar-brand mx-3" href="#">
+            <img src="img/logo.png" width="70" alt="">
+        </a>
+        <h5 class="mx-5">INSTITUT TEKNOLOGI<br>BISNIS AAS<br>INDONESIA</h5>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto mx-3">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">BERANDA</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">PROFIL</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">AKREDITASI</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
